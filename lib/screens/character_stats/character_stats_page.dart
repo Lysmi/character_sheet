b1f03f58283e16e76107ddd2634181a04f19a867
Lysmi <lysmi.mx@mail.ru>
2022-03-30 09:48:36 +0300

    Provider add

--- lib/screens/character_stats/character_stats_page.dart
+++ lib/screens/character_stats/character_stats_page.dart
@@ -11,7 +11,9 @@ import 'package:character_sheet/screens/character_stats/widgets/proficiency_bonu
 import 'package:character_sheet/screens/character_stats/widgets/saving_throws.dart';
 import 'package:character_sheet/screens/character_stats/widgets/skill_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../data/models/provider/character_provider_model.dart';
 import '../../styles/colours.dart';
 
 class CharacterStatsContent extends StatelessWidget {
@@ -20,87 +22,90 @@ class CharacterStatsContent extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("William"),
-        backgroundColor: currTheme.mainColor,
-        toolbarHeight: 42,
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-          constraints: const BoxConstraints(maxWidth: 500),
-          child: ListView(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            children: [
-              Column(
-                children: [
-                  Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    spacing: 13,
-                    children: const [
-                      CharacterPhoto(),
-                      Info(),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Wrap(
-                      spacing: 8,
+    return ChangeNotifierProvider(
+      create: (context) => CharacterProviderModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("William"),
+          backgroundColor: currTheme.mainColor,
+          toolbarHeight: 42,
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+            constraints: const BoxConstraints(maxWidth: 500),
+            child: ListView(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              children: [
+                Column(
+                  children: [
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      spacing: 13,
                       children: const [
-                        Inspiration(),
-                        ProficiencyBonus(),
+                        CharacterPhoto(),
+                        Info(),
                       ],
                     ),
-                  ),
-                  Wrap(
-                    spacing: 9,
-                    runSpacing: 9,
-                    children: [
-                      Row(
-                        children: [
-                          const AbilityListView(),
-                          const SizedBox(
-                            width: 24,
-                          ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                const DerivedStats(),
-                                const Health(),
-                                const HitDice(),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 10),
-                                  child: Row(
-                                    children: [
-                                      Wrap(
-                                        spacing: 9,
-                                        direction: Axis.vertical,
-                                        children: const [
-                                          DeathSave(),
-                                          PassivePerception(),
-                                        ],
-                                      ),
-                                      const Expanded(
-                                        child: Padding(
-                                          padding: EdgeInsets.only(left: 8.0),
-                                          child: SavingThrows(),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Wrap(
+                        spacing: 8,
+                        children: const [
+                          Inspiration(),
+                          ProficiencyBonus(),
                         ],
                       ),
-                      const SkillListView(),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                    ),
+                    Wrap(
+                      spacing: 9,
+                      runSpacing: 9,
+                      children: [
+                        Row(
+                          children: [
+                            const AbilityListView(),
+                            const SizedBox(
+                              width: 24,
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  const DerivedStats(),
+                                  const Health(),
+                                  const HitDice(),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 10),
+                                    child: Row(
+                                      children: [
+                                        Wrap(
+                                          spacing: 9,
+                                          direction: Axis.vertical,
+                                          children: const [
+                                            DeathSave(),
+                                            PassivePerception(),
+                                          ],
+                                        ),
+                                        const Expanded(
+                                          child: Padding(
+                                            padding: EdgeInsets.only(left: 8.0),
+                                            child: SavingThrows(),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SkillListView(),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
