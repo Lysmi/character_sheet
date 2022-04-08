2eb6178398530b0cdefb1e6eaa2f4fe9851f9158
Lysmi <lysmi.mx@mail.ru>
2022-04-08 03:30:08 +0300

    feat: feat entity added

---
+++ lib/domain/entities/feat_entity.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class FeatEntity {
+  String name;
+  String description;
+
+  FeatEntity({
+    required this.name,
+    required this.description,
+  });
+}
