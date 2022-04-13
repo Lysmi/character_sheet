c4bdac646f5c1d7964a3fc38a41204d18b66d828
Lysmi <lysmi.mx@mail.ru>
2022-04-13 06:26:29 +0300

    feat: saving throws add

---
+++ lib/presentation/screens/character_stats/widgets/saving_throw_item.dart
@@ -0,0 +1,47 @@
+import 'package:character_sheet/core/string_convertation.dart';
+import 'package:character_sheet/presentation/styles/colours.dart';
+import 'package:character_sheet/presentation/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../domain/entities/ability_entity.dart';
+
+class SavingThrowItem extends StatelessWidget {
+  final int value;
+
+  final Abilities ability;
+
+  final bool proficiency;
+
+  const SavingThrowItem(
+      {Key? key, required this.ability, required this.value, this.proficiency = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 23,
+      child: Row(
+        children: [
+          Text(
+            abilityToString(ability, context),
+            style: listItemTextStyle,
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: proficiency
+                  ? Text(
+                      addPlusToInt(value),
+                      style: listItemTextStyle.copyWith(color: currTheme.accentMainColor),
+                    )
+                  : Text(
+                      addPlusToInt(value),
+                      style: listItemTextStyle,
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/screens/character_stats/widgets/saving_throws.dart
+++ lib/presentation/screens/character_stats/widgets/saving_throws.dart
@@ -1,30 +1,87 @@
+import 'package:character_sheet/domain/entities/ability_entity.dart';
+import 'package:character_sheet/presentation/screens/character_stats/widgets/saving_throw_item.dart';
+import 'package:character_sheet/presentation/screens/provider_models/character_provider_model.dart';
 import 'package:character_sheet/presentation/styles/colours.dart';
 import 'package:character_sheet/presentation/styles/global_styles.dart';
 import 'package:character_sheet/presentation/styles/text_styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SavingThrows extends StatelessWidget {
   const SavingThrows({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 194,
-      padding: const EdgeInsets.symmetric(horizontal: 9),
-      decoration: BoxDecoration(
-        color: currTheme.secondaryColor,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-        boxShadow: const [cardShadow],
-      ),
-      child: Column(
-        children: [
-          Text(
-            "Saving Throws",
-            style: titleTextStyle,
+    return Consumer<CharacterProviderModel>(
+      builder: (context, character, child) {
+        return Container(
+          height: 194,
+          padding: const EdgeInsets.symmetric(horizontal: 9),
+          decoration: BoxDecoration(
+            color: currTheme.secondaryColor,
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            boxShadow: const [cardShadow],
           ),
-          Row()
-        ],
-      ),
+          child: Column(
+            children: [
+              Text(
+                "Saving Throws",
+                style: titleTextStyle,
+              ),
+              Column(
+                children: [
+                  SavingThrowItem(
+                    ability: Abilities.strength,
+                    value: character.getSavingThrowValue(Abilities.strength),
+                    proficiency: character.getSavingThrowProficiency(Abilities.strength),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  SavingThrowItem(
+                    ability: Abilities.dexterity,
+                    value: character.getSavingThrowValue(Abilities.dexterity),
+                    proficiency: character.getSavingThrowProficiency(Abilities.dexterity),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  SavingThrowItem(
+                    ability: Abilities.constitution,
+                    value: character.getSavingThrowValue(Abilities.constitution),
+                    proficiency: character.getSavingThrowProficiency(Abilities.constitution),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  SavingThrowItem(
+                    ability: Abilities.intelligence,
+                    value: character.getSavingThrowValue(Abilities.intelligence),
+                    proficiency: character.getSavingThrowProficiency(Abilities.intelligence),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  SavingThrowItem(
+                    ability: Abilities.wisdom,
+                    value: character.getSavingThrowValue(Abilities.wisdom),
+                    proficiency: character.getSavingThrowProficiency(Abilities.wisdom),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  SavingThrowItem(
+                    ability: Abilities.charisma,
+                    value: character.getSavingThrowValue(Abilities.charisma),
+                    proficiency: character.getSavingThrowProficiency(Abilities.charisma),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
