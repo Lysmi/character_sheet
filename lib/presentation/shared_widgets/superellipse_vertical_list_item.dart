953948548d189535c84bb69500212d3b8a3b7caf
Lysmi <lysmi.mx@mail.ru>
2022-11-01 03:57:22 +0100

    merge: git merge fix

--- lib/presentation/shared_widgets/superellipse_vertical_list_item.dart
+++ lib/presentation/shared_widgets/superellipse_vertical_list_item.dart
@@ -1,44 +1,50 @@
-import 'package:character_sheet/presentation/shared_widgets/superellipse_vertical_list_item_decorator.dart';
+import 'dart:html';
+
+import 'package:character_sheet/core/column_element_position_enum.dart';
+import 'package:character_sheet/presentation/shared_widgets/superellipse_vertical_list.dart';
 import 'package:flutter/material.dart';
 
-class SuperellipseVerticalListItem extends Container {
-  SuperellipseVerticalListItem({
-    Key? key,
-    AlignmentGeometry? alignment,
-    EdgeInsetsGeometry? padding,
-    Color? color,
-    SuperellipseVerticalListItemDecoration? decoration,
-    Decoration? foregroundDecoration,
-    this.width,
-    this.height,
-    BoxConstraints? constraints,
-    EdgeInsetsGeometry? margin,
-    Matrix4? transform,
-    AlignmentGeometry? transformAlignment,
-    Widget? child,
-    Clip clipBehavior = Clip.none,
-  }) : super(
-            key: key,
-            alignment: alignment,
-            padding: padding,
-            color: color,
-            decoration: decoration ?? SuperellipseVerticalListItemDecoration(),
-            foregroundDecoration: foregroundDecoration,
-            width: width,
-            height: height,
-            constraints: constraints,
-            margin: margin,
-            transform: transform,
-            transformAlignment: transformAlignment,
-            child: child,
-            clipBehavior: clipBehavior) {
-    decorationSettings =
-        decoration; // TODO это тоже должно уйти с появлением гигабилдера
+class SuperelipseVerticalListItem extends StatelessWidget {
+  const SuperelipseVerticalListItem(
+      {this.columnPosition = SuperelipseVerticalListItemPositionEnum.single,
+      this.borderRadius = SuperelipseVerticalList.defaultBorderRadius,
+      // this.height = SuperelipseColumn.defaultElementHeight,
+      this.child,
+      Key? key})
+      : super(key: key);
+
+  /// Не стоит это использовать если ты не знаешь что делаешь
+  final SuperelipseVerticalListItemPositionEnum columnPosition;
+  final double borderRadius;
+  // final double height;
+  final Widget? child;
+
+  BorderRadius getBorderRadiusByElementPosition(
+      SuperelipseVerticalListItemPositionEnum position) {
+    switch (position) {
+      case SuperelipseVerticalListItemPositionEnum.first:
+        return BorderRadius.vertical(
+            top: Radius.circular(borderRadius), bottom: Radius.zero);
+
+      case SuperelipseVerticalListItemPositionEnum.middle:
+        return const BorderRadius.all(Radius.zero);
+
+      case SuperelipseVerticalListItemPositionEnum.last:
+        return BorderRadius.vertical(
+            top: Radius.zero, bottom: Radius.circular(borderRadius));
+
+      case SuperelipseVerticalListItemPositionEnum.single:
+        return BorderRadius.all(Radius.circular(borderRadius));
+    }
   }
 
-  final double? width;
-  final double? height;
-  SuperellipseVerticalListItemDecoration?
-      decorationSettings; // TODO пока так, что бы иметь возможность вытащить это в superellipseCerticalList
-  // TODO надо изменить логику на билдер, хуилдер
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: getBorderRadiusByElementPosition(columnPosition),
+      ),
+      child: child,
+    );
+  }
 }
