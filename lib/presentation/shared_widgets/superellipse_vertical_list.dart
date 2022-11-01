4b49a2df192715bacc4e1b4bf2df7f770d2d39b3
Lysmi <lysmi.mx@mail.ru>
2022-11-01 03:53:25 +0100

    feat: superellipseVerticalList v.1.5

--- lib/presentation/shared_widgets/superellipse_vertical_list.dart
+++ lib/presentation/shared_widgets/superellipse_vertical_list.dart
@@ -1,71 +1,62 @@
 import 'package:character_sheet/core/column_element_position_enum.dart';
 import 'package:character_sheet/presentation/shared_widgets/superellipse_vertical_list_item.dart';
-import 'package:character_sheet/presentation/shared_widgets/superellipse_vertical_list_item_decorator.dart';
 import 'package:flutter/cupertino.dart';
 
 class SuperelipseVerticalList extends Column {
-  static const double defaultBorderRadiusValue = 10;
+  static const double defaultBorderRadius = 10;
   // static const double defaultElementHeight = 26;
-  static const double defaultIdent = 3;
+  static const double defaultIndent = 3;
 
   SuperelipseVerticalList({
-    Key? key,
-    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
-    MainAxisSize mainAxisSize = MainAxisSize.max,
-    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
-    TextDirection? textDirection,
-    VerticalDirection verticalDirection = VerticalDirection.down,
-    TextBaseline? textBaseline,
-    List<SuperellipseVerticalListItem> children =
-        const <SuperellipseVerticalListItem>[],
-
-    /// Будут ли элемента списка скругляться согласно логики этого виджета, хз
-    bool automaticItemBorderRadiusSelection = true,
+    double borderRadius = defaultBorderRadius,
+    double indent = defaultIndent,
+    VoidCallback elementOnTap = avoid,
+    super.mainAxisAlignment,
+    super.mainAxisSize,
+    super.crossAxisAlignment,
+    super.textDirection,
+    super.verticalDirection,
+    super.textBaseline,
+    List<Widget> children = const [],
+    super.key,
   }) : super(
-            // TODO это определнно нужно сделать через билдер с особым юзеджем иначе кринж пересозданий -оптимизация за такое отдельный котёл
-            key: key,
-            mainAxisAlignment: mainAxisAlignment,
-            mainAxisSize: mainAxisSize,
-            crossAxisAlignment: crossAxisAlignment,
-            textDirection: textDirection,
-            verticalDirection: verticalDirection,
-            textBaseline: textBaseline,
-            children: automaticItemBorderRadiusSelection
-                ? children
-                    .asMap()
-                    .entries
-                    .map((entry) => SuperellipseVerticalListItem(
-                          key: entry.value.key,
-                          alignment: entry.value.alignment,
-                          padding: entry.value.padding,
-                          color: entry.value.color,
-                          decoration: SuperellipseVerticalListItemDecoration(
-                              color: entry.value.decorationSettings?.color,
-                              image: entry.value.decorationSettings?.image,
-                              border: entry.value.decorationSettings?.border,
-                              borderRadiusValue: entry.value.decorationSettings
-                                      ?.borderRadiusValue ??
-                                  SuperelipseVerticalList
-                                      .defaultBorderRadiusValue,
-                              boxShadow:
-                                  entry.value.decorationSettings?.boxShadow,
-                              gradient:
-                                  entry.value.decorationSettings?.gradient,
-                              shape: entry.value.decorationSettings?.shape ??
-                                  BoxShape.rectangle,
-                              backgroundBlendMode: entry.value
-                                  .decorationSettings?.backgroundBlendMode),
-                          foregroundDecoration:
-                              entry.value.foregroundDecoration,
-                          width: entry.value.width,
-                          height: entry.value.height,
-                          constraints: entry.value.constraints,
-                          margin: entry.value.margin,
-                          transform: entry.value.transform,
-                          transformAlignment: entry.value.transformAlignment,
-                          child: entry.value.child,
-                          clipBehavior: entry.value.clipBehavior,
-                        ))
-                    .toList()
-                : children);
+            children: children
+                .asMap()
+                .entries
+                .map((entry) => (elementOnTap != avoid
+                    ? GestureDetector(
+                        onTap: elementOnTap,
+                        child: SuperelipseVerticalListItem(
+                          borderRadius: borderRadius,
+                          columnPosition: children.length >
+                                  1 // TODO мож сделать по человечески?)
+                              ? entry.key > 1
+                                  ? entry.key == children.length - 1
+                                      ? SuperelipseVerticalListItemPositionEnum
+                                          .middle
+                                      : SuperelipseVerticalListItemPositionEnum
+                                          .last
+                                  : SuperelipseVerticalListItemPositionEnum
+                                      .first
+                              : SuperelipseVerticalListItemPositionEnum.single,
+                          child: entry.value,
+                        ),
+                      )
+                    : SuperelipseVerticalListItem(
+                        borderRadius: borderRadius,
+                        columnPosition: children.length >
+                                1 // TODO мож сделать по человечески?)
+                            ? entry.key > 1
+                                ? entry.key == children.length - 1
+                                    ? SuperelipseVerticalListItemPositionEnum
+                                        .middle
+                                    : SuperelipseVerticalListItemPositionEnum
+                                        .last
+                                : SuperelipseVerticalListItemPositionEnum.first
+                            : SuperelipseVerticalListItemPositionEnum.single,
+                        child: entry.value,
+                      )))
+                .toList());
 }
+
+void avoid() {}
